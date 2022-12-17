e45aa313c0f2e8a5d1c63e2e1a173fec97727b52
Thecoolwolf2017 <34423413+Thecoolwolf2017@users.noreply.github.com>
2022-12-16 18:19:31 -0700

    Update config.lua

--- hh_aidoc[ESX]/config.lua
+++ hh_aidoc[ESX]/config.lua
@@ -6,4 +6,10 @@ Config = {}
 Config.Doctor = 0 -- Minimum Amount of EMS to work
 Config.Price = 2000
 Config.ReviveTime = 20000  --in msec
-
+Config.disableMovement = false
+Config.disableCarMovement = false
+Config.disableMouse = false 
+Config.disableCombat = false
+Config.doctormodal = "s_m_m_doctor_01"
+Config.carmodal = "ambulance"
+Config.plate = "Doctor"
